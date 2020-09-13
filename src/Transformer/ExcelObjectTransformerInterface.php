d5cf52067ba4683cc60e454506398d4d95e0066a
Nur Hidayatullah <kematjaya0@gmail.com>
2020-09-13 23:37:09 +0700

    update import excel

--- src/Transformer/ExcelObjectTransformerInterface.php
+++ src/Transformer/ExcelObjectTransformerInterface.php
@@ -7,9 +7,17 @@ namespace Kematjaya\ReportBundle\Transformer;
  */
 interface ExcelObjectTransformerInterface extends ObjectTransformerInterface
 {
-    const CONSTRAINT_REQUIRED = 'required';
-    const CONSTRAINT_UNIQUE = 'unique';
-    const CONSTRAINT_REFERENCE_CLASS = 'reference_class';
+    const CONSTRAINT_REQUIRED           = 'required';
+    const CONSTRAINT_UNIQUE             = 'unique';
+    const CONSTRAINT_REFERENCE_CLASS    = 'reference_class';
+    const CONSTRAINT_REFERENCE_FIELD    = 'reference_field';
+    
+    const CONSTRAINT_TYPE_NUMBER        = 'number';
+    
+    const KEY_INDEX           = 'index';
+    const KEY_CONSTRAINT      = 'constraint';
+    const KEY_FIELD           = 'field';
+    const KEY_TYPE            = 'type';
     
     public function startReadedRow():int;
     
